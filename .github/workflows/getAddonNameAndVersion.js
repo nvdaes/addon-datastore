38a539cf1a3d0921caaf8152413f9799d7612352
Noelia Ruiz Martínez <nrm1977@gmail.com>
2023-10-17 12:44:45 +0200

    Print context

--- .github/workflows/getAddonNameAndVersion.js
+++ .github/workflows/getAddonNameAndVersion.js
@@ -1,12 +1,13 @@
 module.exports = ({context, core}) => {
-  const fs = require('fs')
-  const addonFilename = context.needs.getAddonFilename.outputs.addonFileName
-  const contents = fs.readFileSync(addonFilename)
-  const metadata = JSON.parse(contents)
-  const addonId = metadata.addonId
-  core.setOutput('addonId', addonId)
-  const addonName = metadata.displayName
-  core.setOutput('addonName', addonName)
-  const addonVersion = metadata.addonVersionName
-  core.setOutput('addonVersion', addonVersion)
-}
+  const fs = require('fs');
+  console.log(context);
+  const addonFilename = context.needs.getAddonFilename.outputs.addonFileName;
+  const contents = fs.readFileSync(addonFilename);
+  const metadata = JSON.parse(contents);
+  const addonId = metadata.addonId;
+  core.setOutput('addonId', addonId);
+  const addonName = metadata.displayName;
+  core.setOutput('addonName', addonName);
+  const addonVersion = metadata.addonVersionName;
+  core.setOutput('addonVersion', addonVersion);
+};
