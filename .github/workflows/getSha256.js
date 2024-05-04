06ea467c959dd0ab4a78b126202de03c2730aa04
Noelia Ruiz Martínez <nrm1977@gmail.com>
2024-05-04 06:19:14 +0200

    Get sha256 with javascript

---
+++ .github/workflows/getSha256.js
@@ -0,0 +1,10 @@
+module.exports = ({core}, path) => {
+  const fs = require('fs');
+  const crypto = require('crypto');
+  const contents = fs.readFileSync(path);
+  const hash = crypto.createHash('sha256');
+  hash.update(contents);
+  const hex = hash.digest('hex');
+  core.setOutpot('sha256', hex);
+  console.log(hex);
+};
