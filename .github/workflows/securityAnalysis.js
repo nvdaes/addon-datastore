ee19b2d4337fb01f447fc95b0cb2a7cdc322fbdd
Noelia Ruiz Martínez <nrm1977@gmail.com>
2024-05-04 20:01:12 +0200

    Fix

--- .github/workflows/securityAnalysis.js
+++ .github/workflows/securityAnalysis.js
@@ -1,9 +1,13 @@
-module.exports = ({core}, path) => {
+module.exports = ({core}, path, shouldAnalize) => {
   const fs = require('fs');
   const contents = fs.readFileSync(path);
   const data = JSON.parse(contents);
   const runs = data.runs[0];
   const results = runs.results;
+  if (shouldAnalize === false) {
+	  core.setInfo('Analysis overruled')
+	  return
+  }
   if (results.length === 0) {
     core.info("Security analysis succeeded");
   } else {
