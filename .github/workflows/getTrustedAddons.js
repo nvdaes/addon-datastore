827a748d1c779a92df3b7a5e1d2fe11027d1e9a5
Noelia Ruiz Martínez <nrm1977@gmail.com>
2024-05-05 12:09:28 +0200

    Use two jobs for analysis

--- .github/workflows/getTrustedAddons.js
+++
@@ -1,6 +0,0 @@
-module.exports = (path) => {
-  const fs = require('fs');
-  const contents = fs.readFileSync(path);
-  const data = JSON.parse(contents);
-  return data.trustedAddons;
-};
---
+++ .github/workflows/shouldBeAnalized.js
@@ -0,0 +1,8 @@
+module.exports = (path, sha256) => {
+  const fs = require('fs');
+  const contents = fs.readFileSync(path);
+  const data = JSON.parse(contents);
+  if (data.trustedAddons.includes(sha256)) {
+    core.setFailed('SHA-256 included in trusted add-ons');
+  }
+};
