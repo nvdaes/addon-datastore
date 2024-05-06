36d42efe4a32ea0f3cb0eb74e7a26cb7bd34b3b8
Noelia Ruiz Martínez <nrm1977@gmail.com>
2024-05-06 11:05:56 +0200

    Fix

--- .github/workflows/shouldBeAnalized.js
+++ .github/workflows/shouldBeAnalized.js
@@ -3,9 +3,6 @@ module.exports = ({core}, path, sha256) => {
   const contents = fs.readFileSync(path);
   const data = JSON.parse(contents);
   if (data.trustedAddons.includes(sha256)) {
-    core.setInfo('SHA-256 included in trusted add-ons');
-    core.setOutput('sha256', 'included');
-  } else {
-    core.setOutput('sha256', 'excluded');
+    core.setFailed('Analysis not required')
   }
 };
