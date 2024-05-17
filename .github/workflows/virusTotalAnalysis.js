420af4ab8997a206b1d7e046dd2b6a70fec44327
Noelia Ruiz Martínez <nrm1977@gmail.com>
2024-05-17 05:09:01 +0200

    Fix

--- .github/workflows/virusTotalAnalysis.js
+++ .github/workflows/virusTotalAnalysis.js
@@ -5,15 +5,21 @@ module.exports = ({core, exec}) => {
   const addonId = addonMetadata.addonId;
   core.setOutput('addonId', addonId);
   const sha256 = addonMetadata.sha256;
-  exec.exec(`vt file ${sha256} -k ${process.env.API_KEY} --format json > vt.json`);
+  let vtOutput = '';
+  const options = {};
+  options.listeners = {
+    stdout: (data: Buffer) => {
+      vtOutput += data.toString();
+    }
+  };
+  exec.exec(`vt file ${sha256} -k ${process.env.API_KEY} --format json`, options);
   const falsePositiveAddonsContents = fs.readFileSync('falsePositiveAddons.json');
   const falsePositiveAddonsData = JSON.parse(falsePositiveAddonsContents);
   if (falsePositiveAddonsData[addonId] !== undefined && falsePositiveAddonsData[addonId].includes(sha256)) {
     core.info('VirusTotal false positive');
     return;
   }
-const vtContents = fs.readFileSync('vt.json');
-  const vtData = JSON.parse(vtContents);
+  const vtData = JSON.parse(options.listeners.vtOutput);
   const stats = vtData.find((element) => element === "last_analysis_stats");
   const malicious = stats.malicious;
   if (malicious === 0) {
