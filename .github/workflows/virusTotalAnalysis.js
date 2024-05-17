8fbfebe7e3a588e0d5d446177ebcc5931d38a3aa
Noelia Ruiz Martínez <nrm1977@gmail.com>
2024-05-17 09:36:53 +0200

    Fix

--- .github/workflows/virusTotalAnalysis.js
+++ .github/workflows/virusTotalAnalysis.js
@@ -8,13 +8,14 @@ module.exports = ({core}) => {
   const sha256 = addonMetadata.sha256;
   exec(`vt file ${sha256} -k ${process.env.API_KEY} --format json`, (err, stdout, stderr) => {
     console.log(stdout);
+    const vtData = JSON.parse(stdout);
+    const stats = vtData.find((element) => element === "last_analysis_stats");
+    const malicious = stats.malicious;
+    if (malicious === 0) {
+      core.info("Virus Total analysis succeeded");
+    } else {
+      fs.writeFileSync('vt.json', stdout);
+      core.setFailed("Virus Total analysis failed");
+    }
   });
-  const vtData = JSON.parse(stdout);
-  const stats = vtData.find((element) => element === "last_analysis_stats");
-  const malicious = stats.malicious;
-  if (malicious === 0) {
-    core.info("Virus Total analysis succeeded");
-  } else {
-    core.setFailed("Virus Total analysis failed");
-  }
 };
